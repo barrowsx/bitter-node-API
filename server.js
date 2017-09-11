f9639e0c3b4484c2261dc492181f0aa6b21f6266
barrowsx <wwboyer14@mail.strakejesuit.org>
2017-09-11 13:03:14 -0400

    Chats are now dynamically made into two user rooms

--- server.js
+++ server.js
@@ -1,55 +1,59 @@
 const io = require('socket.io')()
 const mongo = require('mongodb').MongoClient
+const MONGODB_URI = process.env.MONGODB_URI
+let chatName = ''
 
 io.on('connection', socket => {
   console.log('a user connected')
 
   socket.on('create chat', (user, currentUser) => {
-    let chatName = user.toString() + '_' + currentUser.toString()
-    mongo.connect(process.env.MONGODB_URI, (err, db) => {
+    chatName = user.toString() + '_' + currentUser.toString()
+    mongo.connect(MONGODB_URI, (err, db) => {
       let collection = db.collection(chatName)
       db.listCollections({name: chatName}).next((err, collInfo) => {
         if(collInfo){
           console.log('chatroom ' + chatName + ' already exists, not creating...')
-          socket.emit('chat exists')
         } else {
-          collection.insert({user: '', content: 'Welcome to the chat!', date: Date.now()}, (err, o) => {
-            if(err){
-              console.warn(err.message)
-            } else {
-              console.log('chatroom ' + chatName + ' created.')
-            }
-          })
+          db.createCollection(chatName)
+          console.log('chatroom ' + chatName + ' created')
         }
       })
     })
   })
 
-  // socket.on('chat connect')
-
-  mongo.connect(process.env.MONGODB_URI, (err, db) => {
-    let collection = db.collection('chat messages')
-    let stream = collection.find().sort({date: -1}).limit(10).stream()
-    stream.on('data', chat => {
-      socket.emit('chat', chat)
+  socket.on('room', room => {
+    console.log('joining room', room)
+    chatName = room
+    socket.join(room)
+    mongo.connect(MONGODB_URI, (err, db) => {
+      let collection = db.collection(room)
+      let stream = collection.aggregate([
+        //sort({date: -1}).limit(10)
+        {'$sort': {date: -1}},
+        {'$limit': 10},
+        {'$sort': {date: 1}}
+      ]).stream()
+      stream.on('data', chat => {
+        socket.emit('load chat', chat, room)
+      })
     })
   })
 
-  socket.on('chat', (usr, msg, date) => {
-    mongo.connect(process.env.MONGODB_URI, (err, db) => {
-      let collection = db.collection('chat messages')
+
+  socket.on('chat', (usr, msg, date, chatRoom) => {
+    mongo.connect(MONGODB_URI, (err, db) => {
+      let collection = db.collection(chatRoom)
       collection.insert({user: usr, content: msg, date: date}, (err, o) => {
         if(err){
           console.warn(err.message)
         } else {
-          console.log(usr + " chat message inserted into db: " + msg + " with timestamp " + date)
+          console.log(usr + " chat message " + msg + " inserted into db: " + chatRoom + " with timestamp " + date)
         }
       })
     })
-    io.emit('chat', {user: usr, content: msg})
-  })
-  socket.on('disconnect', () => {
-    console.log('a user disconnected')
+    console.log(chatRoom)
+    console.log('emitting message', msg, 'from user', usr, 'in room', chatRoom)
+    io.to(chatRoom).emit('chat', {room: chatRoom, user: usr, content: msg})
   })
 })
 
