73c2d185e3336aba9f34b69c3370bf38a4ca879b
barrowsx <wwboyer14@mail.strakejesuit.org>
2017-09-10 20:14:35 -0400

    Initial Commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const io = require('socket.io')()
+const mongo = require('mongodb').MongoClient
+
+io.on('connection', socket => {
+  console.log('a user connected')
+
+  socket.on('create chat', (user, currentUser) => {
+    let chatName = user.toString() + '_' + currentUser.toString()
+    mongo.connect('mongodb://heroku_t3sxklnk:gb3bklkpgabn9pmp6m828j11rb@ds129394.mlab.com:29394/heroku_t3sxklnk', (err, db) => {
+      let collection = db.collection(chatName)
+      db.listCollections({name: chatName}).next((err, collInfo) => {
+        if(collInfo){
+          console.log('chatroom ' + chatName + ' already exists, not creating...')
+          socket.emit('chat exists')
+        } else {
+          collection.insert({user: '', content: 'Welcome to the chat!', date: Date.now()}, (err, o) => {
+            if(err){
+              console.warn(err.message)
+            } else {
+              console.log('chatroom ' + chatName + ' created.')
+            }
+          })
+        }
+      })
+    })
+  })
+
+  // socket.on('chat connect')
+
+  mongo.connect('mongodb://heroku_t3sxklnk:gb3bklkpgabn9pmp6m828j11rb@ds129394.mlab.com:29394/heroku_t3sxklnk', (err, db) => {
+    let collection = db.collection('chat messages')
+    let stream = collection.find().sort({date: -1}).limit(10).stream()
+    stream.on('data', chat => {
+      socket.emit('chat', chat)
+    })
+  })
+
+  socket.on('chat', (usr, msg, date) => {
+    mongo.connect('mongodb://heroku_t3sxklnk:gb3bklkpgabn9pmp6m828j11rb@ds129394.mlab.com:29394/heroku_t3sxklnk', (err, db) => {
+      let collection = db.collection('chat messages')
+      collection.insert({user: usr, content: msg, date: date}, (err, o) => {
+        if(err){
+          console.warn(err.message)
+        } else {
+          console.log(usr + " chat message inserted into db: " + msg + " with timestamp " + date)
+        }
+      })
+    })
+    io.emit('chat', {user: usr, content: msg})
+  })
+  socket.on('disconnect', () => {
+    console.log('a user disconnected')
+  })
+})
+
+const port = 3002
+io.listen(port)
+console.log('listening on port', port)
